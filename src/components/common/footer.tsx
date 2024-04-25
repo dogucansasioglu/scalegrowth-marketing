a3d49dfc5fea5cfb7c5a6a378e284d935a07bc17
Doğucan Şaşıoğlu <sasiogludogucan@gmail.com>
2024-04-26 00:57:47 +0300

    linkedin social, pb footer

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -4,7 +4,7 @@ import {
   SocialLink,
   type SocialLinkType,
 } from "@/components/common/social-link";
-import { Github, Instagram, Twitter } from "lucide-react";
+import { Github, Instagram, Linkedin, Twitter } from "lucide-react";
 
 const socials: SocialLinkType[] = [
   {
@@ -19,11 +19,15 @@ const socials: SocialLinkType[] = [
     Icon: Github,
     href: "https://github.com/dogucansasioglu",
   },
+  {
+    Icon: Linkedin,
+    href: "https://www.linkedin.com/in/dogucan-sasioglu/",
+  },
 ];
 
 export function Footer() {
   return (
-    <Container className="mt-40 flex flex-col gap-20">
+    <Container className="mt-40 flex flex-col gap-20 pb-10">
       <div className="h-0.5 w-full bg-secondary"></div>
       <div className="flex justify-between">
         <div>
