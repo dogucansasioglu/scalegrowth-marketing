6a5aacfcd54fbd04191b8a1ec1e061cb07950aa5
Doğucan Şaşıoğlu <sasiogludogucan@gmail.com>
2024-05-19 01:53:35 +0300

    offer angle update

--- src/app/trainings/_components/hero.tsx
+++ src/app/trainings/_components/hero.tsx
@@ -28,13 +28,13 @@ export function Hero() {
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
   return (
-    <Container className="mt-0 flex min-h-[90vh] items-center justify-center">
+    <Container className="mt-0 flex min-h-[80vh] items-center justify-center">
       <div className="flex flex-col items-center justify-center gap-8 text-center">
         <div className="rounded-full border bg-[#ebecff] p-4 text-center font-bold uppercase shadow-lg sm:text-lg">
-          For AGENCY OWNERS, COACHES & CONSULTANTS
+          Attention AGENCY OWNERS, COACHES & CONSULTANTS
         </div>
         <h1 className="text-4xl font-bold sm:text-5xl md:text-6xl lg:text-7xl">
-          Download All Of Our Trainings For Free
+          Get All Trainings For Free Now
         </h1>
         <div className="mt-3 max-w-4xl text-balance text-xl">
           Get instant access to our trainings covering paid ads, content, cold
