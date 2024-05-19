391748ac7aa20115eac996ef069a377b6dad2183
Doğucan Şaşıoğlu <sasiogludogucan@gmail.com>
2024-05-20 02:23:01 +0300

    thank you page after booking a call

---
+++ src/app/thank-you/_components/hero.tsx
@@ -0,0 +1,35 @@
+import { Container } from "@/components/common/container";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export function Hero() {
+  return (
+    <Container className="mt-0 flex min-h-[80vh] items-center justify-center pt-10">
+      <div className="flex flex-col items-center justify-center gap-8 text-center">
+        <div className="rounded-full border bg-[#ebecff] p-4 text-center font-bold uppercase shadow-lg sm:text-lg">
+          You&apos;re Booked!
+        </div>
+        <h1 className="text-4xl font-bold sm:text-5xl md:text-6xl lg:text-7xl">
+          Get Ready to <br className="hidden md:block" /> Transform Your
+          Business
+        </h1>
+        <div className="mt-3 max-w-4xl text-balance text-xl">
+          Thanks for scheduling a call! While you wait, join our free Skool
+          community. Connect with like-minded pros, access exclusive trainings,
+          and get expert support.
+        </div>
+        <div className="flex flex-col gap-2">
+          <Link
+            href={"https://calendly.com/sasiogludogucan/discovery"}
+            target="_blank"
+          >
+            <Button size={"xl"} className="mx-auto w-fit">
+              Join the Free Skool Community Now
+            </Button>
+          </Link>
+          <div className="text-center italic">Free to join.</div>
+        </div>
+      </div>
+    </Container>
+  );
+}
---
+++ src/app/thank-you/page.tsx
@@ -0,0 +1,9 @@
+import { Hero } from "@/app/thank-you/_components/hero";
+
+export default async function Trainings() {
+  return (
+    <div className="flex flex-col gap-32">
+      <Hero />
+    </div>
+  );
+}
